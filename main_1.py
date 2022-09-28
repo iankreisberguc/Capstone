c2a5d531071aadd72c256d68bd7715f0adde7006
joaquin-alvarez21 <joaquin.alvarez@uc.cl>
2022-09-27 21:21:59 -0300

    Actualizando

--- main_1.py
+++ main_1.py
@@ -1,8 +1,9 @@
 import pandas as pd 
 from clases import Bay, Barco, Container
 from funciones import generar_espacios, calcular_centro_masa,\
-    over_stowage, calcular_valor, verificar_esfuerfos_de_corte
-from primera_carga import crear_primera_solucion
+    over_stowage, calcular_valor, calcular_esfuerzos_corte
+from prueba import crear_primera_solucion
+
 
 data_barco = pd.read_excel('container_ship_data.xlsx', 'Ship_bays_estr_data', skiprows=4, usecols="C:I", header=1)
 data_loaded = pd.read_excel('container_ship_data.xlsx', 'Loaded_containers_data')
@@ -11,10 +12,8 @@ data_hydrostatic = pd.read_excel('container_ship_data.xlsx', 'Ship_hydrostatic_d
 data_buoyancy = pd.read_excel('container_ship_data.xlsx', 'Ship_buoyancy_data', skiprows=4, usecols="C:X", header=1)
 data_cargamento = pd.read_excel("container_ship_data.xlsx","Loading_list_data")
 
-
 valor_kpi = 50
 data_cargamento_filtrada = data_cargamento[data_cargamento["KPI"] > valor_kpi].reset_index()
-data_prueba = data_cargamento_filtrada.groupby("END_PORT", group_keys=False).apply(lambda x: x).sort_values(by=["END_PORT", "VALUE (USD)"], ascending=False)
 barco = Barco()
 
 barco.generar_bays(data_barco)
@@ -34,22 +33,32 @@ for index, row in data_loaded.iterrows():
     vcg = float(data_slot[(data_slot.BAY==bay) & (data_slot.STACK==stack) & (data_slot.TIER==tier) & (data_slot.SLOT==1)].VCG)
     container = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
 
-    if bay == 15 and stack == 1:
-        if barco.bays[int(bay)].espacio[int(tier)][int(stack)][int(slot)-1] == None:
-            print('Revisa el codigo que hay un error pq hay un comteiner en una posicion invalida')
-        
-        else:
-            barco.bays[int(bay)].espacio[int(tier)][int(stack)][int(slot)-1] = container  
+    if barco.bays[int(bay)].espacio[int(tier)][int(stack)][int(slot)-1] == None:
+        print('Revisa el codigo que hay un error')
+    
+    else:
+        barco.bays[int(bay)].espacio[int(tier)][int(stack)][int(slot)-1] = container  
 
-# print(barco.bays[15].espacio)
-# print(calcular_centro_masa(barco))
+data_prueba = data_cargamento_filtrada.groupby("END_PORT", group_keys=False).apply(lambda x: x).sort_values(by=["END_PORT", "VALUE (USD)"], ascending=False)
+data_RC = data_prueba[data_prueba["TYPE"] == "RC"]
+data_DG = data_prueba[data_prueba["TYPE"] == "DG"]
+data_DC = data_prueba[data_prueba["TYPE"] == "DC"]
 
-crear_primera_solucion(data_prueba, data_slot, barco)
+# print(data_prueba)
+# print(data_RC.iloc[0]['VALUE (USD)'])
+# print(data_DG)
+# print(len(data_DG))
+# print(data_DC)
 
+# crear_primera_solucion(data_prueba, data_slot, barco)
+
+# contador_bay = 0
 # for bay in barco.bays:
 #     for tier in bay.espacio:
-#         print(tier)
-
-resultado_caso_base = calcular_valor(barco) - over_stowage(barco)*60
-
-print(resultado_caso_base)
+#         for stack in tier:
+#             if stack[1] not in [0, 1, 2, None]:
+#                 if stack[0].es_cargado and stack[1].es_cargado and contador_bay == 15:
+#                     print(stack[0].largo, stack[0].tipo, stack[0].end_port)
+#                     print(stack[1].largo, stack[1].tipo, stack[1].end_port)
+#                     print("-"*50)
+#     contador_bay += 1
---
+++ primera_carga.py
@@ -0,0 +1,142 @@
+from clases import Container
+
+def crear_primera_solucion (data_prueba, data_slot, barco):
+    
+    data_RC = data_prueba[data_prueba["TYPE"] == "RC"]
+    data_DG = data_prueba[data_prueba["TYPE"] == "DG"]
+    data_DC = data_prueba[data_prueba["TYPE"] == "DC"]
+
+    contador_bay = 0
+
+    contador_RC = 0
+    contador_DG = 0
+    contador_DC = 0
+    for bay in barco.bays:
+        contador_tier = 0
+        for tier in bay.espacio:
+            contador_stack = 0
+            for slot in tier:
+                for contenedor in slot:
+                    if slot[0] in [0,1,2] and slot[1] in [0,1,2]:
+                        if contenedor == 1 and contador_RC < len(data_RC):
+                            peso = data_RC.iloc[contador_RC]['WEIGHT (ton)']
+                            tipo = data_RC.iloc[contador_RC]['TYPE']
+                            valor = data_RC.iloc[contador_RC]['VALUE (USD)']
+                            es_cargado = False
+                            end_port = data_RC.iloc[contador_RC]['END_PORT']
+                            largo = data_RC.iloc[contador_RC]['LENGTH (ft)']
+                            tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                            vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                            contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                            barco.bays[contador_bay].espacio[contador_tier][contador_stack][0] = contenedor
+                            contador_RC += 1
+                    
+                        elif contenedor == 2 and contador_DG < len(data_DG):
+                            peso = data_DG.iloc[contador_DG]['WEIGHT (ton)']
+                            tipo = data_DG.iloc[contador_DG]['TYPE']
+                            valor = data_DG.iloc[contador_DG]['VALUE (USD)']
+                            es_cargado = False
+                            end_port = data_DG.iloc[contador_DG]['END_PORT']
+                            largo = data_DG.iloc[contador_DG]['LENGTH (ft)']
+                            tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                            vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                            contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                            barco.bays[contador_bay].espacio[contador_tier][contador_stack][0] = contenedor
+                            contador_DG += 1
+                    
+                        elif contenedor == 0 and contador_DC < len(data_DC):
+                            peso = data_DC.iloc[contador_DC]['WEIGHT (ton)']
+                            tipo = data_DC.iloc[contador_DC]['TYPE']
+                            valor = data_DC.iloc[contador_DC]['VALUE (USD)']
+                            es_cargado = False
+                            end_port = data_DC.iloc[contador_DC]['END_PORT']
+                            largo = data_DC.iloc[contador_DC]['LENGTH (ft)']
+                            tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                            vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                            contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                            barco.bays[contador_bay].espacio[contador_tier][contador_stack][0] = contenedor
+                            contador_DC += 1
+
+                    elif slot[0] in [0,1,2] and slot[1] !=None:
+                        if slot[1].largo != 40:
+                            if contenedor == 1 and contador_RC < len(data_RC):
+                                peso = data_RC.iloc[contador_RC]['WEIGHT (ton)']
+                                tipo = data_RC.iloc[contador_RC]['TYPE']
+                                valor = data_RC.iloc[contador_RC]['VALUE (USD)']
+                                es_cargado = False
+                                end_port = data_RC.iloc[contador_RC]['END_PORT']
+                                largo = data_RC.iloc[contador_RC]['LENGTH (ft)']
+                                tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                                vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                                contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                                barco.bays[contador_bay].espacio[contador_tier][contador_stack][0] = contenedor
+                                contador_RC += 1
+                        
+                            elif contenedor == 2 and contador_DG < len(data_DG):
+                                peso = data_DG.iloc[contador_DG]['WEIGHT (ton)']
+                                tipo = data_DG.iloc[contador_DG]['TYPE']
+                                valor = data_DG.iloc[contador_DG]['VALUE (USD)']
+                                es_cargado = False
+                                end_port = data_DG.iloc[contador_DG]['END_PORT']
+                                largo = data_DG.iloc[contador_RC]['LENGTH (ft)']
+                                tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                                vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                                contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                                barco.bays[contador_bay].espacio[contador_tier][contador_stack][0] = contenedor
+                                contador_DG += 1
+                            
+                            elif contenedor == 0 and contador_DC < len(data_DC):
+                                peso = data_DC.iloc[contador_DC]['WEIGHT (ton)']
+                                tipo = data_DC.iloc[contador_DC]['TYPE']
+                                valor = data_DC.iloc[contador_DC]['VALUE (USD)']
+                                es_cargado = False
+                                end_port = data_DC.iloc[contador_DC]['END_PORT']
+                                largo = data_DC.iloc[contador_DC]['LENGTH (ft)']
+                                tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                                vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                                contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                                barco.bays[contador_bay].espacio[contador_tier][contador_stack][0] = contenedor
+                                contador_DC += 1
+                    elif slot[1] in [0,1,2] and slot[0] != None:
+                        if slot[0].largo != 40:
+                            if contenedor == 1 and contador_RC < len(data_RC):
+                                peso = data_RC.iloc[contador_RC]['WEIGHT (ton)']
+                                tipo = data_RC.iloc[contador_RC]['TYPE']
+                                valor = data_RC.iloc[contador_RC]['VALUE (USD)']
+                                es_cargado = False
+                                end_port = data_RC.iloc[contador_RC]['END_PORT']
+                                largo = data_RC.iloc[contador_RC]['LENGTH (ft)']
+                                tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                                vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                                contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                                barco.bays[contador_bay].espacio[contador_tier][contador_stack][1] = contenedor
+                                contador_RC += 1
+                        
+                            elif contenedor == 2 and contador_DG < len(data_DG):
+                                peso = data_DG.iloc[contador_DG]['WEIGHT (ton)']
+                                tipo = data_DG.iloc[contador_DG]['TYPE']
+                                valor = data_DG.iloc[contador_DG]['VALUE (USD)']
+                                es_cargado = False
+                                end_port = data_DG.iloc[contador_DG]['END_PORT']
+                                largo = data_DG.iloc[contador_RC]['LENGTH (ft)']
+                                tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                                vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                                contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                                barco.bays[contador_bay].espacio[contador_tier][contador_stack][1] = contenedor
+                                contador_DG += 1
+                            
+                            elif contenedor == 0 and contador_DC < len(data_DC):
+                                peso = data_DC.iloc[contador_DC]['WEIGHT (ton)']
+                                tipo = data_DC.iloc[contador_DC]['TYPE']
+                                valor = data_DC.iloc[contador_DC]['VALUE (USD)']
+                                es_cargado = False
+                                end_port = data_DC.iloc[contador_DC]['END_PORT']
+                                largo = data_DC.iloc[contador_DC]['LENGTH (ft)']
+                                tcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].TCG)
+                                vcg = float(data_slot[(data_slot.BAY==contador_bay) & (data_slot.STACK==contador_stack) & (data_slot.TIER==contador_tier) & (data_slot.SLOT==1)].VCG)
+                                contenedor = Container(peso, tipo, valor, end_port, largo, tcg, vcg, es_cargado)
+                                barco.bays[contador_bay].espacio[contador_tier][contador_stack][1] = contenedor
+                                contador_DC += 1
+                contador_stack += 1
+            contador_tier += 1
+        contador_bay += 1 
